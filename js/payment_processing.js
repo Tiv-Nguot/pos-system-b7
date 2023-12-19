7b7fafc795cbe6d2b76491cf8e923b0d1428cf86
TIV.NGUOT <TIV.NGUOT@PNC.LAN>
2023-12-20 06:07:36 +0700

    #20 design for payment

---
+++ js/payment_processing.js
@@ -0,0 +1,6 @@
+// const data = localStorage.getItem('products');
+// const products_data = JSON.parse(data);
+// console.log('products_data', products_data)
+
+let button = document.querySelector('.print-button');
+
